50ee3bada6b6a2dcef2da0e0bd607d38760c12cd
Manoj Srivastava <srivasta@debian.org>
2006-02-27 19:50:26 +0000

    Typo fix.
    Typo fix.
    
    
    git-archimport-id: srivasta@debian.org--etch/kernel-package--devel--9.0--patch-129

--- kernel/ruleset/targets/headers.mk
+++ kernel/ruleset/targets/headers.mk
@@ -107,7 +107,7 @@ ifeq ($(strip $(MAKING_VIRTUAL_IMAGE)),)
                            $(SRCDIR)/arch/$(KERNEL_ARCH)/kernel/asm-offsets.s
 	for file in $(localversion_files) dummy; do                               \
           test ! -e $$file || $(install_file) $$file $(SRCDIR);                   \
-        fi
+        done
 	$(install_file) .config  	        $(SRCDIR)/.config
 	echo $(debian)                    > $(SRCDIR)/$(INT_STEM)-headers.revision
 	sed -e 's/=V/$(version)/g'    -e 's/=IB/$(link_in_boot)/g'   \
