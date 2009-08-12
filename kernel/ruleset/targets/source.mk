2e8cad01bcedbb7d2a602058763f2231c78c68b9
Manoj Srivastava <srivasta@debian.org>
2009-08-12 09:40:08 -0500

    Update documents to reflect new capabilities and conventions
    Various small updates to man pages, and removals of documents that are
    now obsolete.
    
    Closes: 540363
    
    Signed-Off-By: Manoj Srivastava <srivasta@debian.org>

--- kernel/ruleset/targets/source.mk
+++ kernel/ruleset/targets/source.mk
@@ -1,6 +1,6 @@
 ######################### -*- Mode: Makefile-Gmake -*- ########################
-## source.mk --- 
-## Author           : Manoj Srivastava ( srivasta@glaurung.internal.golden-gryphon.com ) 
+## source.mk ---
+## Author           : Manoj Srivastava ( srivasta@glaurung.internal.golden-gryphon.com )
 ## Created On       : Mon Oct 31 13:55:32 2005
 ## Created On Node  : glaurung.internal.golden-gryphon.com
 ## Last Modified By : Manoj Srivastava
@@ -8,12 +8,12 @@
 ## Last Machine Used: anzu.internal.golden-gryphon.com
 ## Update Count     : 14
 ## Status           : Unknown, Use with caution!
-## HISTORY          : 
-## Description      : This file is responsible forcreating the kernel-source packages 
-## 
+## HISTORY          :
+## Description      : This file is responsible forcreating the kernel-source packages
+##
 ## arch-tag: 1a7fd804-128f-4f9d-9e3d-ce6bdb731823
-## 
-## 
+##
+##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2 of the License, or
@@ -31,7 +31,7 @@
 ###############################################################################
 
 
-debian/stamp/install/$(s_package): 
+debian/stamp/install/$(s_package):
 	$(REASON)
 	@echo "This is kernel package version $(kpkg_version)."
 	rm -rf $(TMPTOP)
@@ -49,8 +49,6 @@ debian/stamp/install/$(s_package):
 	$(install_file) README                         $(DOCDIR)/README
 	$(install_file) debian/changelog               $(DOCDIR)/changelog.Debian
 	$(install_file) $(DEBDIR)/docs/README          $(DOCDIR)/debian.README
-	$(install_file) $(DEBDIR)/docs/README.grub     $(DOCDIR)/
-	$(install_file) $(DEBDIR)/docs/README.tecra    $(DOCDIR)/
 	$(install_file) $(DEBDIR)/docs/README.modules  $(DOCDIR)/
 	$(install_file) $(DEBDIR)/docs/Rationale       $(DOCDIR)/
 	$(install_file) $(DEBDIR)/examples/sample.module.control               \
@@ -63,7 +61,7 @@ debian/stamp/install/$(s_package):
 	echo "This was produced by kernel-package version $(kpkg_version)." >  \
 	                                               $(DOCDIR)/Buildinfo
 ######################################################################
-#### 
+####
 ######################################################################
 ifneq ($(strip $(int_follow_symlinks_in_src)),)
 	-tar cfh - $$(echo * | sed -e 's/ debian//g' -e 's/\.deb//g' ) |       \
@@ -82,7 +80,7 @@ endif
          [ ! -d scripts/cramfs ]   || make -C scripts/cramfs distclean ; )
 	sed -e 's/=V/$(KERNELRELEASE)/g' -e 's/=A/$(DEB_HOST_ARCH)/g'           \
              -e 's/=ST/$(INT_STEM)/g'  -e 's/=B/$(KERNEL_ARCH)/g'               \
-                 $(DEBDIR)/pkg/source/README >  $(SRCDIR)/README.Debian ;       
+                 $(DEBDIR)/pkg/source/README >  $(SRCDIR)/README.Debian ;
 	if test -f README.Debian ; then                                         \
            $(install_file) README.Debian        $(DOCDIR)/README.Debian.1st;    \
 	   gzip -9qf                            $(DOCDIR)/README.Debian.1st;    \
